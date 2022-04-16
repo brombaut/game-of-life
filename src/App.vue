4c84aaf7080c48e11cf9cb9208e5a9f5246f8f71
Ben Rombaut <rombaut.benj@gmail.com>
2022-04-16 19:38:57 -0400

    Committing before adding testing

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,43 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <GameGrid />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import GameGrid from './components/GameGrid.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    GameGrid,
+  },
 });
 </script>
 
 <style lang="scss">
+
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  margin: 0;
+  height: 100vh;
+}
+h1,h2,h3,h4,h5,h6 {
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/GameGrid.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="game-grid">
+    <div
+      v-for="i in numberOfCellRows"
+      :key="i"
+      class="grid-row">
+      <div
+        v-for="j in numberOfCellCols"
+        :key="j"
+        class="cell">
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: "GameGrid",
+  data() {
+    return {
+      cellHeight: null,
+    };
+  },
+  computed: {
+    numberOfCellRows() {
+      return 10;
+    },
+    numberOfCellCols() {
+      return 20;
+    }
+  }
+});
+</script>
+
+<style lang="scss">
+#game-grid {
+  flex: 1;
+
+  .cell {
+    border: 1px solid black;
+  }
+}
+
+</style>
